a243edf440616d95f21b8f35c959ab7fea0f43f7
System Administrator <root@MacBook-Air-Sergey.local>
2015-03-17 16:03:38 +0300

    kt4sf login

--- PageObj.js
+++ PageObj.js
@@ -8,12 +8,26 @@ var loginKTMPage = function() {
 	this.txtUsername = element(by.id('loginUsername'));
 	this.txtPassword = element(by.id('loginPassword'));
 	this.btnLogin = element(by.id('loginBtn'));
+	
 	//get start page
 	this.getLandingPage = function() {
 		browser.get("https://manager-stable.knowledgetree.com");
 		//TODO - replace with proper waitforpagetoload
 		waits(12000);
 	}
+	
+	this.getSFLandingPage = function() {
+		browser.driver.get("https://login.salesforce.com");
+		//TODO - replace with proper waitforpagetoload
+		waits(12000);
+	}
+	
+	this.SFlogin = function(username, password) {
+		//fill username and password, click login
+		browser.driver.findElement(by.id('username')).sendKeys(username);
+		browser.driver.findElement(by.id('password')).sendKeys(password);
+		browser.driver.findElement(by.id('Login')).click();
+		}
 
 	//login into popup window	
 	this.loginAsSFUser = function(SFusername, SFpassword){		
@@ -21,21 +35,16 @@ var loginKTMPage = function() {
 		this.btnLoginSF.click();
 		
 		runs(function() {
+			//getting handles of all browser windows
 			var handles = browser.driver.getAllWindowHandles().then(function (handles) {
-		    var h2 = handles[1];
-		    var h1 = handles[0];
-		    
-		    expect(h2).toBeDefined();	
-			console.log(h1);
-			console.log(h2);
-			console.log(handles.length);
-			
-			parenth = h1;
+		    //to be ure that authentication window has defined handle
+		    var h2 = handles[1];	    
+		    expect(h2).toBeDefined();
+			//switch to authentication window, fill username and password, click login
 			browser.driver.switchTo().window(h2);
 			browser.driver.findElement(By.id('username')).sendKeys(SFusername);
 			browser.driver.findElement(By.id('password')).sendKeys(SFpassword);
 			browser.driver.findElement(By.id('Login')).click();
-			waits(12000);
 			return handlesDone = true;
 			});
 		});
@@ -43,18 +52,25 @@ var loginKTMPage = function() {
 		waitsFor(function(){
 			return handlesDone;
 			});
-
 	}
 	
 	this.loginWithUsername = function(username, password){		
 		//click login as sf user btn
 		this.btnLoginUsername.click();
+		//fill username and password, click login
 		this.txtUsername.sendKeys(username);
 		this.txtPassword.sendKeys(password);
 		this.btnLogin.click();
 	}
-		
+	
+	this.closeBrowserWnd = function(){
+		browser.executeScript('window.close()');
+	}
+	
+	this.validateLoggedInProperly = function(){
+		//TODO: add validation here that user's 1st name and family name displays properly,
+		//all menu items are visible, URL is correct, etc.
+	}		
 }
-
 		
 module.exports = new loginKTMPage();
--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 exports.config  = {
     //
-    allScriptsTimeout: 15000,
+    allScriptsTimeout: 25000,
     getPageTimeout: 15000,
     //running selenium server address
     seleniumAddress: 'http://localhost:4444/wd/hub',
 
     //capabilities: browsers
     capabilities: {
-    'browserName': 'safari'
+    'browserName': 'chrome'
     },
 
     //list our tests here here
@@ -15,6 +15,6 @@ exports.config  = {
     //options
     jasmineNodeOpts: {
         showcolors: true,
-        defaultTimeoutInterval: 15000
+        defaultTimeoutInterval: 25000
     }
 };
